27370309d5eea13183ce94b8a04f45ae90f73e9c
Luca Park <qlife1146@gmail.com>
2025-06-04 14:49:33 +0900

    필수 과제 2 커밋

--- 3weeks/main.swift
+++ 3weeks/main.swift
@@ -7,5 +7,21 @@
 
 import Foundation
 
-//필수1
-caculate(closure: sum)
+// 필수 1
+caculate(closure: sum) // '두 수의 합은 30 입니다'
+
+// 필수 2-1
+result = numbers.map { String($0) }
+print(result) // '["1", "2", "3", "4", "5"]'
+
+// 필수 2-2
+print(type(of: requireTwoNumbers)) // 'Array<Int>'
+let requireTwoResult = requireTwoFilter.map { String($0) }
+print(type(of: requireTwoResult)) // 'Array<String>'
+
+// 필수 2-3
+let reqiureThreeResult = myMap([1, 2, 3, 4, 5]) {
+    String($0)
+}
+
+print(reqiureThreeResult) // '["1", "2", "3", "4", "5"]'
