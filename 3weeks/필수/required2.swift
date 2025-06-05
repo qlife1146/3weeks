bddfa8b52c166367c9281a155f9544267f1fd641
Luca Park <qlife1146@gmail.com>
2025-06-05 20:15:30 +0900

    도전 1 커밋

---
+++ 3weeks/필수/required2.swift
@@ -0,0 +1,48 @@
+//
+//  required2.swift
+//  3weeks
+//
+//  Created by Luca Park on 6/4/25.
+//
+
+import Foundation
+
+// 아래 for-in 문을 map 을 사용하는 코드로 변환해주세요.
+// let numbers = [1, 2, 3, 4, 5]
+// var result = [String]()
+// for number in numbers {
+//  result.append(number)
+// }
+// ​
+// 주어진 입력값을 고차함수를 체이닝하여 주어진 출력값이 나오도록 구현해주세요.
+// 입력: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] - 타입: Array<Int>
+// 출력: [”2”, “4”, “6”, “8”, “10”] - 타입: Array<String>
+// 힌트
+// 이제 고차함수를 직접 만들어 봅니다.
+// 함수명: myMap
+// 파라미터는 2개 입니다.
+// 배열: [Int]
+// 변환 클로저: (Int) → String
+// 반환 값은 1개 입니다.
+// [String]
+// myMap 함수 구현 내부에서 map, filter, reduce 등 고차함수를 직접 사용하지 않아야합니다.
+// 완성된 myMap 호출 예시
+// let result = myMap([1, 2, 3, 4, 5]) {
+//    String($0)
+// }
+//
+// print(result) // ["1", "2", "3", "4", "5"]
+
+let numbers = [1, 2, 3, 4, 5]
+var result = [String]()
+
+let requireTwoNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+var requireTwoFilter = requireTwoNumbers.filter { $0 % 2 == 0 }
+
+func myMap(_ array: [Int], _ result: (Int) -> String) -> [String] {
+    var resultArray: [String] = []
+    for i in array {
+        resultArray.append(String(i))
+    }
+    return resultArray
+}
---
+++ 3weeks/필수/required4.swift
@@ -0,0 +1,113 @@
+//
+//  required4.swift
+//  3weeks
+//
+//  Created by Luca Park on 6/5/25.
+//
+
+import Foundation
+
+// 우리는 여러 타입에 “자기소개” 기능을 부여하고자 합니다.
+//
+// - [ ]  Introducible 프로토콜을 정의하세요.
+//    - name: String 프로퍼티를 요구사항으로 포함합니다.
+//    - introduce() -> String 메서드를 요구사항으로 포함합니다.
+//        - 동작 예시: print("안녕하세요, 저는 \(name)입니다.")
+// - [ ]  Robot, Cat, Dog 타입을 정의하고 Introducible 프로토콜을 채택해주세요.
+// - [ ]  Robot 타입의 경우, name 값이 변경될 때마다 변경 이전값과 이후 값을 출력하도록 구현해주세요.
+//    - 만약 변경 이전값과 이후값이 같다면 출력하지 않아야합니다.
+//    - 출력 예시
+//        ```swift
+//        name 변경 알림
+//        변경 이전 값: 피규어
+//        변경 이후 값: 옵티머스
+//        ```
+// - [ ]  Introducible 프로토콜 에 정의되지 않은 각 타입 고유의 메서드들도 하나씩 추가 정의해주세요.
+//    - 예를 들어, Robot 은 충전하기(batteryCharge) 라는 메서드를 추가 할 수 있습니다.
+// - [ ]  `[Introducible]` 타입 배열을 정의하고, Robot, Cat, Dog 인스턴스 1개씩을 append 해주세요.
+//    - 배열을 순회하며 각 타입 고유의 메서드들을 호출하는 코드를 작성해주세요.
+
+var skillList: [Introducible] = []
+
+protocol Introducible {
+    var name: String { get }
+    func introduce() -> String // print("안녕하세요, 저는 \(name)입니다.")
+}
+
+struct Robot: Introducible {
+    // 프로퍼티 옵저버
+    var name: String {
+        didSet {
+            if oldValue != name {
+                print("name 변경 알림")
+                print("변경 이전 값: \(oldValue)")
+                print("변경 이후 값: \(name)")
+            }
+        }
+    }
+
+    func introduce() -> String {
+        return "안녕하세요, 저는 \(name)입니다."
+    }
+
+    func batteryCharge() -> String {
+        return "충전 중입니다."
+    }
+}
+
+struct Cat: Introducible {
+    var name: String
+
+    func introduce() -> String {
+        return "안녕하세요, 저는 \(name)입니다."
+    }
+
+    func purr() -> String {
+        return "그르릉"
+    }
+
+    init(name: String) {
+        self.name = name
+    }
+}
+
+struct Dog: Introducible {
+    var name: String
+
+    func introduce() -> String {
+        return "안녕하세요, 저는 \(name)입니다."
+    }
+
+    func bark() -> String {
+        return "댕댕"
+    }
+
+    init(name: String) {
+        self.name = name
+    }
+}
+
+func robotCall() {
+    var robot = Robot(name: "피규어")
+    robot.name = "옵티머스"
+    robot.name = "옵티머스"
+}
+
+func skillAppend() {
+    skillList.append(Robot(name: "피규어"))
+    skillList.append(Cat(name: "끙끙이"))
+    skillList.append(Dog(name: "도토리"))
+}
+
+func skillPrint() {
+    skillAppend()
+    for skill in skillList {
+        if let robot = skill as? Robot {
+            print(robot.batteryCharge())
+        } else if let cat = skill as? Cat {
+            print(cat.purr())
+        } else if let dog = skill as? Dog {
+            print(dog.bark())
+        }
+    }
+}
